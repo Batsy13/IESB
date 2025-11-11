118968f7a95c683e72761e38a7bf85e367cb5c7b
Pedro Costa <pedrocostavgs@gmail.com>
2025-11-11 14:55:08 -0300

    finish final exercise for openmp

--- parallel-programming/openmp/008_final_exercise.cpp
+++ parallel-programming/openmp/008_final_exercise.cpp
@@ -2,10 +2,58 @@
 #include <omp.h>
 #include <algorithm>
 #include <vector>
+#include <cmath>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+double calculate_coeficiente_variacao_sequential(int N, const std::vector<int>& fi, const std::vector<double>& xi) {
+    double sum_xi_fi = 0.0;
+    int k = fi.size();
+
+    for (int i = 0; i < k; i++) {
+        sum_xi_fi += xi[i] * fi[i];
+    }
+
+    double mu = sum_xi_fi / N;
+
+    double sum_desvio_padrao = 0.0;
+    for (int i = 0; i < k; i++) {
+        sum_desvio_padrao += fi[i] * pow(xi[i] - mu, 2);
+    }
+
+    double sigma = sqrt(sum_desvio_padrao / N);
+    
+    double coeficiente_variacao = (sigma / mu) * 100.0;
+    return coeficiente_variacao;
+}
+
+double calculate_coeficiente_variacao_parallel(int N, const std::vector<int>& fi, const std::vector<double>& xi) {
+    double sum_xi_fi = 0.0;
+    int k = fi.size();
+
+    #pragma omp parallel for reduction(+:sum_xi_fi)
+    for (int i = 0; i < k; i++) {
+        sum_xi_fi += xi[i] * fi[i];
+    }
+
+    double mu = sum_xi_fi / N;
+    
+    double sum_desvio_padrao = 0.0;
+    #pragma omp parallel for reduction(+:sum_desvio_padrao)
+    for (int i = 0; i < k; i++) {
+        sum_desvio_padrao += fi[i] * pow(xi[i] - mu, 2);
+    }
+
+    double sigma = sqrt(sum_desvio_padrao / N);
+    
+    double coeficiente_variacao = (sigma / mu) * 100.0;
+    return coeficiente_variacao;
+}
 
 int main()
 {
-
+    srand(time(NULL));
     const int NUM_PEOPLE = 1000;
     std::vector<int> altura(NUM_PEOPLE);
     std::vector<float> peso(NUM_PEOPLE);
@@ -13,57 +61,91 @@ int main()
     for (int i = 0; i < NUM_PEOPLE; i++)
     {
         altura[i] = rand() % 60 + 140;
-        peso[i] = rand() % 100 + 30;
+        peso[i] = (float)(rand() % 1000) / 10.0 + 30.0;
     }
     
     std::sort(altura.begin(), altura.end());
     std::sort(peso.begin(), peso.end());
     
-    int menor_altura = altura[1];
-    int maior_altura = altura[NUM_PEOPLE - 1];
-
-    std::cout << menor_altura << std::endl
-              << maior_altura << std::endl;
-
     std::vector<int> fi_altura;
+    std::vector<double> xi_altura;
     std::vector<int> fi_peso;
-    std::vector<int> xi_altura;
-    std::vector<int> xi_peso;
+    std::vector<double> xi_peso;
 
-    // Para altura ( intervalos de 8 )
+    int min_altura = 140;
+    int max_altura = 200;
+    int step_altura = 8;
 
-    for (int i = 140; i < 200; i += 8)
+    float min_peso = 30.0f;
+    float max_peso = 130.0f;
+    float step_peso = 4.0f;
+
+    for (int i = min_altura; i < max_altura; i += step_altura)
     {
         int pessoas_no_intervalo = 0;
         for (int j = 0; j < NUM_PEOPLE; j++)
         {
-            if (altura[j] >= i && altura[j] < i + 8)
+            if (altura[j] >= i && altura[j] < i + step_altura)
             {
-                pessoas_no_intervalo += 1;
+                pessoas_no_intervalo++;
             }
-            else if (altura[j] < i)
-            {
-                continue;
-            }
-            else
+            else if (altura[j] >= i + step_altura)
             {
                 break;
             }
         }
-        std::cout << "Pessoas no intervalo: " << i << " |-- " << i + 8 << ": " << pessoas_no_intervalo << std::endl;
-        fi_altura.push_back(pessoas_no_intervalo);
-        xi_altura.push_back((i + i + 8)/2);
+        if (pessoas_no_intervalo > 0) {
+            fi_altura.push_back(pessoas_no_intervalo);
+            xi_altura.push_back((i + i + step_altura) / 2.0);
+        }
     }
 
-    float mi_altura = 0;
-
-    for(int i = 0; i < NUM_PEOPLE; i++){
-        mi_altura += fi_altura[i] * xi_altura[i];
+    for (float i = min_peso; i < max_peso; i += step_peso)
+    {
+        int pessoas_no_intervalo = 0;
+        for (int j = 0; j < NUM_PEOPLE; j++)
+        {
+            if (peso[j] >= i && peso[j] < i + step_peso)
+            {
+                pessoas_no_intervalo++;
+            }
+            else if (peso[j] >= i + step_peso)
+            {
+                break;
+            }
+        }
+        if (pessoas_no_intervalo > 0) {
+            fi_peso.push_back(pessoas_no_intervalo);
+            xi_peso.push_back((i + i + step_peso) / 2.0);
+        }
     }
 
-    float media_altura = mi_altura;
+    std::cout << std::fixed << std::setprecision(4);
 
-    std::cout << media_altura << std::endl;
+    double start_seq = omp_get_wtime();
+    double coeficiente_variacao_altura_seq = calculate_coeficiente_variacao_sequential(NUM_PEOPLE, fi_altura, xi_altura);
+    double coeficiente_variacao_peso_seq = calculate_coeficiente_variacao_sequential(NUM_PEOPLE, fi_peso, xi_peso);
+    double end_seq = omp_get_wtime();
+    double time_seq = end_seq - start_seq;
+
+    double start_par = omp_get_wtime();
+    double coeficiente_variacao_altura_par = calculate_coeficiente_variacao_parallel(NUM_PEOPLE, fi_altura, xi_altura);
+    double coeficiente_variacao_peso_par = calculate_coeficiente_variacao_parallel(NUM_PEOPLE, fi_peso, xi_peso);
+    double end_par = omp_get_wtime();
+    double time_par = end_par - start_par;
+
+    std::cout << "\n--- Resultados Sequenciais ---" << std::endl;
+    std::cout << "coeficiente_variacao Altura: " << coeficiente_variacao_altura_seq << "%" << std::endl;
+    std::cout << "coeficiente_variacao Peso: " << coeficiente_variacao_peso_seq << "%" << std::endl;
+    std::cout << "Tempo Sequencial: " << time_seq << " segundos" << std::endl;
+
+    std::cout << "\n--- Resultados Paralelos (OpenMP) ---" << std::endl;
+    std::cout << "coeficiente_variacao Altura: " << coeficiente_variacao_altura_par << "%" << std::endl;
+    std::cout << "coeficiente_variacao Peso: " << coeficiente_variacao_peso_par << "%" << std::endl;
+    std::cout << "Tempo Paralelo: " << time_par << " segundos" << std::endl;
+
+    std::cout << "\n--- Analise de Desempenho ---" << std::endl;
+    std::cout << "Speedup: " << (time_seq / time_par) << "x" << std::endl << std::endl;
 
     return 0;
 }
