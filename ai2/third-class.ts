2aafa8053f9889e2871b3abc2226ab4779044f34
Batsy13 <pedrocostavgs@gmail.com>
2026-03-11 21:31:13 -0300

    fix third class

--- ai2/third-class.ts
+++ ai2/third-class.ts
@@ -1,3 +1,10 @@
+const population: number[][] = [
+  [1.0, 0.5],
+  [2.0, 2.0],
+  [0.1, 0.2],
+  [3.0, 1.5]
+];
+
 function calculateFitness(population: number[][]): number[] {
 
     let nInd = population.length;
@@ -23,9 +30,25 @@ function objectiveFunction(x: number[]): number {
     return result
 }
 
-let population = [
-    [5, 10, 0],
-    [2, 7, -0.5]
-];
+function rouletteWheelSelection(population: number[][], fitnessScores: number[]): number[] {
+    const totalFitness = fitnessScores.reduce((sum, score) => sum + score, 0);
+  const nInd = population.length;
+  
+  const randomPoint = Math.random() * totalFitness;
+  
+  let cumulativeSum = 0;
+  for (let i = 0; i < nInd; i++) {
+      cumulativeSum += fitnessScores[i];
+      if (cumulativeSum >= randomPoint) {
+      return population[i];
+    }
+}
 
-console.log(calculateFitness(population))
+return population[nInd - 1];
+}
+
+const scores = calculateFitness(population);
+const selectedIndividual = rouletteWheelSelection(population, scores);
+
+console.log("Fitness Scores:", scores);
+console.log("Selected Individual:", selectedIndividual);
